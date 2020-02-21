4ba904195e370f6354e447db17bce0955553a6a4
yangqinjiang-macbookpro <yangqinjiang@qq.com>
2020-02-21 14:56:14 +0800

    模块

--- neural_network/neuralNetwork.py
+++ neural_network/neuralNetwork.py
@@ -1,4 +1,5 @@
-#definition
+import numpy
+# definition
 class neuralNetwork:
     # init neuralNetwork
     def __init__(self,inputnodes,hiddennodes,outputnodes,learningrate):
@@ -6,8 +7,16 @@ class neuralNetwork:
         self.inodes = inputnodes
         self.hnodes = hiddennodes
         self.onodes = outputnodes
+        # 链接权重矩阵 wih (weight of input_hidden) 和who (weight of hidden_output)
+        # weights inside the arrays are w_i_j, where link is from node i to node j in the next layer
+        # w11 w21
+        # w12 w22 etc
+        # 正态概率的随机值, 第三个参数是numpy数组的形状大小
+        self.wih = numpy.random.normal(0.0,pow(self.hnodes, -0.5), (self.hnodes , self.inodes))
+        self.who = numpy.random.normal(0.0, pow(self.onodes, -0.5), (self.onodes, self.hnodes))
         # 学习率
         self.lr = learningrate
+
         pass
 
     # train the neuralNetwork
--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 
 from neural_network.neuralNetwork import neuralNetwork
+import numpy as np
 
 if __name__ == '__main__':
     #输入层,隐藏层,输出层的节点数量
@@ -11,3 +12,7 @@ if __name__ == '__main__':
     #创建neuralNetwork的实例
     n = neuralNetwork(input_nodes,hidden_nodes,output_nodes,learning_rate)
     print(n)
+
+    #初始权重矩阵
+    print(np.random.rand(3,3)) # 3x3的numpy数组,数组中的每个值都是0~1的随机值
+    print(np.random.rand(3,3) - 0.5) # 减去0.5
