0ea511a0684a2fe0c96d201cfc206535c831312c
yangqinjiang-macbookpro <yangqinjiang@qq.com>
2020-02-23 11:00:07 +0800

    完整代码

--- neural_network/neuralNetwork.py
+++ neural_network/neuralNetwork.py
@@ -26,14 +26,35 @@ class neuralNetwork:
         pass
 
     # train the neuralNetwork
-    def train(self):
+    def train(self,inputs_list,targets_list):
+        # convert input list to 2d array
+        inputs = numpy.array(inputs_list, ndmin=2).T
+        targets = numpy.array(targets_list,ndmin=2).T
+
+        # calcualate signals into hidden layer
+        hidden_inputs = numpy.dot(self.wih, inputs)
+        # calculate the signals emerging from hidden layer
+        hidden_outputs = self.activation_function(hidden_inputs)
+        # calculate signals into final output layer
+        final_inputs = numpy.dot(self.who, hidden_outputs)
+        # calculate the signals emerging from final output layer
+        final_outputs = self.activation_function(final_inputs)
+
+        # output layer error is the (target - actual)
+        output_errors = targets - final_outputs
+        # hidden layer error is the output_errors ,split by weights,recombined at hidden nodes
+        hidden_errors = numpy.dot(self.who.T, output_errors)
+        # update the weights for the links between the hidden and output layers
+        self.who += self.lr * numpy.dot((output_errors * final_outputs * (1.0 - final_outputs)) , numpy.transpose(hidden_outputs))
+        # oupdate the weights for the links between the input and hidden layers
+        self.wih += self.lr * numpy.dot((hidden_errors * hidden_outputs * (1.0 - hidden_outputs)) ,numpy.transpose(inputs))
         pass
 
     #query the neuralNetwork
     # 只需要input_list,不需要任何其他输入
-    def query(self,input_list):
+    def query(self,inputs_list):
         # convert input list to 2d array
-        inputs = numpy.array(input_list, ndmin=2).T
+        inputs = numpy.array(inputs_list, ndmin=2).T
         # calcualate signals into hidden layer
         hidden_inputs = numpy.dot(self.wih, inputs)
         # calculate the signals emerging from hidden layer
